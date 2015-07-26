1ccde003a384fedf5b94f1d048b94a52d78b5fea
Shikher Aatrey <shikher.a@gmail.com>
2015-07-26 22:56:14 +0530

    Commit after 1 file ex_30.py addition

---
+++ exercises/ex_17.py
@@ -0,0 +1,10 @@
+people = 10
+cars = 15
+buses = 20
+
+if people > cars:
+	print "No reason to buy a car. We are the majority"
+elif people < cars:
+	print "Lets buy a car."
+else:
+	print "Whatever Dude. I can't figure out what to do."
