04df5bad6a7bb12cbb36bf239e776652a0ca11cc
Shikher Aatrey <shikher.a@gmail.com>
2015-07-30 17:41:28 +0530

    Laptop going down. Had to do this commit

---
+++ exercises/harry_potter_game.py
@@ -0,0 +1,8 @@
+def start():
+	print "In this game, you are Harry Potter and you will be given some choices on each dialogue. \nLet's see where this conversation goes!"
+	print "Ready?"
+
+	ready = raw_input('> ')
+
+	if '1' or 'yes' in ready:	
+		print "\n Scene 1: Hagrid sees Harry for the first after"
